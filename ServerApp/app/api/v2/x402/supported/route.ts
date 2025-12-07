92320b845f7a10fb9e8fa8687a494d666eb80496
Julio M Cruz <julio.cruz@eb-ms.net>
2025-12-07 16:29:42 -0500

    Remove ServerApp directory (renamed to MiddlewareApp)

--- ServerApp/app/api/v2/x402/supported/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { X402Service } from "@/lib/services/X402Service";
-
-const x402Service = new X402Service();
-
-export async function GET() {
-  const result = x402Service.getSupported();
-  return NextResponse.json(result);
-}
